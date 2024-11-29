3f53b0ff70c94fb5ee089e7c590f356758f88a75
Marcus Ng <ngzhengjie888@gmail.com>
2024-11-30 00:46:08 +0800

    deleted link direct to admin page

--- HTML5UMInternalHackaton/public_html/thank_you.php
+++ HTML5UMInternalHackaton/public_html/thank_you.php
@@ -5,8 +5,6 @@
 </head>
 <body>
     
-    <!-- Link to Admin Summary (for CSV download and admin-related tasks) -->
-    <a href="admin.php" class="admin-link">Go to Admin Summary</a>
     <h1>Thank You for Your Order!</h1>
     <p>Your order has been successfully placed and your payment evidence has been uploaded.</p>
 
